3653b501d1ae9990f5c09c7678d35b1b49b3d5fa
djeck1432 <oleh.kostashchuk@gmail.com>
2025-05-31 00:24:57 +0200

    update config file

--- app/core/config.py
+++ app/core/config.py
@@ -1,16 +1,19 @@
 from pydantic import BaseSettings
+from dotenv import load_dotenv
+load_dotenv()
 
 class Settings(BaseSettings):
-    database_url: str
-    redis_url: str
-    web3_provider_uri: str
-    contract_address: str
-    private_key: str
+    # database_url: str
+    # redis_url: str
+    # web3_provider_uri: str
+    # contract_address: str
+    # private_key: str
     github_client_id: str
     github_client_secret: str
     github_callback_url: str
-    secret_key: str
+    # secret_key: str
     algorithm: str = "HS256"
+
 
     class Config:
         env_file = ".env"
