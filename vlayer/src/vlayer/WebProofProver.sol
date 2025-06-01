c1b511b5db8b9a206a36314304b53869ef42a9fb
faurdent <nikitalukianitcares@gmail.com>
2025-06-01 06:09:49 +0200

    Proover and verifier

---
+++ vlayer/src/vlayer/WebProofProver.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Proof} from "vlayer-0.1.0/Proof.sol";
+import {Prover} from "vlayer-0.1.0/Prover.sol";
+import {Web, WebProof, WebProofLib, WebLib} from "vlayer-0.1.0/WebProof.sol";
+
+contract WebProofProver is Prover {
+    using WebProofLib for WebProof;
+    using WebLib for Web;
+
+    string public constant DATA_URL =
+        "https://github.com/djeck1432/spotnet/pull/150/page_data/status_checks";
+
+    function main(WebProof calldata webProof, address account)
+        public
+        view
+        returns (Proof memory, string memory, address)
+    {
+        Web memory web = webProof.verify(DATA_URL);
+
+        string memory screenName = web.jsonGetString("aliveChannels.commitHeadShaChannel");
+
+        return (proof(), screenName, account);
+    }
+}
