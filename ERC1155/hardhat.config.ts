2bced5ffd46ca8aa2ff52bdf7d988b51a95ae4b7
faurdent <nikitalukianitcares@gmail.com>
2025-06-01 05:23:47 +0200

    BlockScout

--- ERC1155/hardhat.config.ts
+++ ERC1155/hardhat.config.ts
@@ -3,10 +3,11 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-network-helpers";
+import "@nomicfoundation/hardhat-foundry";
 
 const { vars } = require("hardhat/config");
 import "@nomicfoundation/hardhat-verify";
-
+import "@nomicfoundation/hardhat-toolbox";
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
@@ -19,18 +20,17 @@ const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 // Beware: NEVER put real Ether into testing accounts
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
-module.exports = {
-  solidity: "0.8.28",
-  networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-    },
-  },
-};
+// module.exports = {
+//   solidity: "0.8.28",
+//   networks: {
+//     sepolia: {
+//       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+//       accounts: [SEPOLIA_PRIVATE_KEY],
+//     },
+//   },
+// };
 
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify";
+const ETH_SCANER = vars.get("ETH_SCAN_PRIVATE_KEY");
 
 const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
@@ -38,18 +38,18 @@ const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     'sepolia': {
-      url: 'https://eth-sepolia.public.blastapi.io',
-      accounts: [PRIVATE_KEY],
+      url: 'https://1rpc.io/sepolia',
+      accounts: [SEPOLIA_PRIVATE_KEY],
     },
   },
   etherscan: {
     apiKey: {
       // Is not required by blockscout. Can be any non-empty string
-      sepolia: "5MDZWAT2IU51338HD6KXG3UI384HKBQKN8"
+      sepolia: ETH_SCANER
     },
     customChains: [
       {
-        network: "eth-sepolia",
+        network: "sepolia",
         chainId: 11155111,
         urls: {
           apiURL: "https://eth-sepolia.blockscout.com/api",
